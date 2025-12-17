63ad7e6d7dcd4f8310c53cb085b1637d4768baca
luongductinh-it <ldt20062003@gmail.com>
2025-12-17 09:01:45 +0700

    feat: Refactor app structure and add meal management features
    - Updated main entry point to use TabsScreen instead of CategoriesScreen.
    - Introduced Category model to represent meal categories.
    - Created Meal model to encapsulate meal details and attributes.
    - Implemented CategoriesScreen to display meal categories and navigate to meals.
    - Added FiltersScreen for managing dietary filters.
    - Developed MealDetailsScreen to show detailed meal information.
    - Created MealsScreen to list meals based on selected categories or favorites.
    - Added TabsScreen to manage navigation between categories and favorites.
    - Implemented CategoryGridItem widget for category display.
    - Created MainDrawer for navigation within the app.
    - Developed MealItem widget to represent individual meals in a list.
    - Added MealItemTrait widget for displaying meal attributes.
    - Updated pubspec.yaml and pubspec.lock for new dependencies and versions.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meals/categories.dart';
+import 'package:meals/screens/tabs.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -22,7 +22,8 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: CategoriesScreen(),
+      debugShowCheckedModeBanner: false,
+      home: TabsScreen(),
     );
   }
 }
